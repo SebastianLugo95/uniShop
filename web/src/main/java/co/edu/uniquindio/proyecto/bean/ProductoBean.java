860b177c586eaac1d68e634df30076222a6dfcfa
StivenHerreraSierra <stiven.herreras@uqvirtual.edu.co>
2021-11-30 16:43:01 -0500

    Carga de imágenes en producto

--- web/src/main/java/co/edu/uniquindio/proyecto/bean/ProductoBean.java
+++ web/src/main/java/co/edu/uniquindio/proyecto/bean/ProductoBean.java
@@ -6,15 +6,20 @@ import co.edu.uniquindio.proyecto.servicios.ProductoServicio;
 import co.edu.uniquindio.proyecto.servicios.UsuarioServicio;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.io.IOUtils;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.file.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
-import java.io.Serializable;
+import java.io.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Component
 @ViewScoped
@@ -30,23 +35,58 @@ public class ProductoBean implements Serializable {
     @Autowired
     private UsuarioServicio usuarioServicio;
 
+    private ArrayList<String> imagenes;
+
+    @Value("${upload.url}")
+    private String urlUploads;
+
     @PostConstruct
     public void inicializar(){
         this.producto = new Producto();
+        this.imagenes = new ArrayList<>();
     }
 
-    public String crearProducto(){
+    public void crearProducto(){
         try {
-            Usuario usuario = usuarioServicio.obtenerUsuario("123");
-            producto.setCodigoVendedor(usuario);
-            productoServicio.publicarProducto(producto);
-            return "productoCreado?faces-redirect=true";
+            if(!imagenes.isEmpty()) {
+                Usuario usuario = usuarioServicio.obtenerUsuario("123");
+                producto.setCodigoVendedor(usuario);
+                producto.setImagenesProducto(imagenes);
+                productoServicio.publicarProducto(producto);
+
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", "Producto creado satisfactoriamente");
+                FacesContext.getCurrentInstance().addMessage(null, fm);
+            } else {
+                FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_WARN, "Alerta", "Es necesario subir al menos  imagen");
+                FacesContext.getCurrentInstance().addMessage(null, fm);
+            }
         } catch (Exception e) {
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Alerta", e.getMessage());
             FacesContext.getCurrentInstance().addMessage(null, fm);
         }
-        return null;
     }
 
+    public void subirImagenes(FileUploadEvent event) {
+        UploadedFile imagen = event.getFile();
+        String nombreImagen = subirImagen(imagen);
+        if(nombreImagen!=null) {
+            imagenes.add(nombreImagen);
+        }
+    }
+
+    public String subirImagen(UploadedFile imagen) {
+        String nombreImagen = null;
+        try {
+            File archivo = new File(urlUploads + "/" + imagen.getFileName());
+            OutputStream os = new FileOutputStream(archivo);
+            IOUtils.copy(imagen.getInputStream(), os);
+
+            nombreImagen = imagen.getFileName();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return nombreImagen;
+    }
 }
 
